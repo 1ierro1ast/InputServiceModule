b16cec1ae9a9c951d49b4402c41423ecdd32c2ce
1ierro1ast <lierro00@gmail.com>
2022-10-26 18:35:21 +0300

    initial commit

---
+++ Assets/Codebase/Infrastructure/Services/Settings/IGameSettings.cs
@@ -0,0 +1,9 @@
+﻿using Codebase.Infrastructure.Services.Input;
+
+namespace Codebase.Infrastructure.Services.Settings
+{
+    public interface IGameSettings : IService
+    {
+        InputSettings InputSettings { get; }
+    }
+}
